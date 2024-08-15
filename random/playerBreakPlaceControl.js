585b07971ab21796dfe3a33ea0942c43cf417214
HedgehogKb <166470267+HedgehogKb@users.noreply.github.com>
2024-08-15 13:04:02 -0400

    adding changes to player breaking

--- random/playerBreakPlaceControl.js
+++ random/playerBreakPlaceControl.js
@@ -1,3 +1,25 @@
-function init(t) {
-    print("test");
-} 
+function interact(t) {
+    //1 is creative mode meaning players can interact with these in creative mode
+    if (!t.player.getGamemode() == 1) {
+        if (t.type == 1) {
+            //var mod = t.target.getName().split(":")[0]
+            if (t.target.getTypeName() == "minecraft:item_frame") {
+                print("canceled");
+                t.setCanceled(true);
+                return;
+            }
+        }
+    }
+}
+
+function broken(t) {
+    if (!t.player.getGamemode() == 1) {
+
+    }
+}
+
+function place(t) {
+    if (!t.player.getGamemode() == 1) {
+        
+    }
+}
